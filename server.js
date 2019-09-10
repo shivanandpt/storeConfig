35793b4f090a895cce52e74560799fb427b0022a
shiva <shivanandpatil.tech@gmail.com>
2019-09-10 08:29:37 +0530

    Add dotenv to get config from .env file
    Signed-off-by: shiva <shivanandpatil.tech@gmail.com>

---
+++ server.js
@@ -0,0 +1,6 @@
+const dotenv = require('dotenv').config();
+
+console.log("port: ", process.env.PORT);
+console.log("port: ", process.env.NODE_ENV);
+console.log("port: ", process.env.DB_HOST);
+console.log("port: ", process.env.API_KEY);
